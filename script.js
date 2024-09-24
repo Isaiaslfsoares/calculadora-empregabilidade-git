d18f64d43a03b0ef813c3ba48f4c58f58a20e25c
Isaias Soares <165928116+Isaiaslfsoares@users.noreply.github.com>
2024-09-23 22:39:29 -0300

    Criando função de Calcular
    Função para calcular o resultado da operação e fazer a verificação de expressão válida

--- script.js
+++ script.js
@@ -20,6 +20,26 @@ function deletar() {
     tela.value = tela.value.slice(0, -1);
 }
 
+function calcularTotal() {
+    const tela = document.formulario.tela;
+    const expressao = tela.value;
+
+    if (expressao && !isOperador(expressao[expressao.length - 1])) {
+        try {
+           
+            const resultado = calcularExpressao(expressao);
+            tela.value = resultado; 
+        } catch (error) {
+            alert('Expressão inválida');
+            limparTela();
+        }
+    }
+}
+
 function isOperador(char) {
     return ['+', '-', '*', '/'].includes(char);
+}
+
+function calcularExpressao(expressao) {    
+    return new Function('return ' + expressao)();
 }
